255593e99a20190f71ced672fd7fcb600c85d885
obigerard18 <obigerard18@gmail.com>
2022-09-20 17:06:57 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_most_numbers - Print numbers from 0 - 9
+* @void: No parameter
+*/
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i == '2' || i == '4')
+			continue;
+		else
+			_putchar(i);
+	}
+	_putchar('\n');
+}
