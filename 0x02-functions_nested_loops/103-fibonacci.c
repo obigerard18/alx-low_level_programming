c1926842ed72faa6549adbd2825e78ce212dea46
obigerard18 <obigerard18@gmail.com>
2022-09-21 05:02:50 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,12 +13,12 @@ int main(void)
 
 	while (j < 4000000)
 	{
-			if (j % 2 == 0)
-				total += j;
+		if (j % 2 == 0)
+			total += j;
 
-			k = j;
-			j += i;
-			i = k;
+		k = j;
+		j += i;
+		i = k;
 	}
 	printf("%d\n", total);
 	return (0);
