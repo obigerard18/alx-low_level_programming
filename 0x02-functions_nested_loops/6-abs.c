7c81b8edcc154ff613dcc6d46ec855eda1d3060b
obigerard18 <obigerard18@gmail.com>
2022-09-20 23:56:29 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _abs - Dispays absolut value
+*
+* int: function parameter
+* @c: accepts integer value
+* Return: return int
+*/
+int _abs(int c)
+{
+	int j;
+
+	j = abs(c);
+	return (j);
+}
