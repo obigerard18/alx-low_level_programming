84e0f7fc219dbc45a2a4bdf668f995179be3b1e1
obigerard18 <obigerard18@gmail.com>
2022-09-20 23:30:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,17 +7,15 @@
 *
 * Return: always 0
 */
-
 int main(void)
 {
-	int i = 0;
-	char c;
-	char s[] = "_putchar\n";
-	while (i <= 8)
-{
-	c = s[i];
-	_putchar(c);
-	i++;
-}
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
