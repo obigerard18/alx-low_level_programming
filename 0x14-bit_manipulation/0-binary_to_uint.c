1f296dc0e694d5749b36350272b84bf8a7592faa
obigerard18 <obigerard18@gmail.com>
2022-10-27 15:18:18 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,44 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* binary_to_uint - function that convert a binary number to an unsigned int
-* @b: string to convert.
+* check_valid_string - checks if a string has only 0's and 1's
+* @b: string to be checked
 *
-* Return: converted number or 0
+* Return: 1 if string is valid, 0 otherwise
 */
+int check_valid_string(const char *b)
+{
+	if (b == NULL)
+		return (0);
+	while (*b)
+	{
+		if (*b != '1' && *b != '0')
+			return (0);
+		b++;
+	}
+
+	return (1);
+}
+/**
+* @b: the binary number as a string
+*
+* Return: the converted value 
+*/ 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, i, expo = 1;
+	unsigned int decimal = 0;
+	int str_len = 0, base = 1;
 
-	if (!b || !*b)
+	if (!check_valid_string(b))
 		return (0);
-
-	i = 0;
-	while (b[i])
+	while (b[str_len] != '\0')
+		str_len++;
+	while (str_len)
 	{
-		if (b[i] - 48 > 1)
-			return (0);
-		i++;
+		decimal += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
 	}
-	i--;
-	num = 0;
-	do {
-		num += (b[i] - '0') * expo;
-		expo *= 2;
-	} while (i--);
-	return (num);
+
+	return (decimal);
 }
